1fa74d59c342ba6e1e7eb653f0778aed9059141e
jessicaeri <jess.eri0926@gmail.com>
2025-02-12 18:43:37 +1000

    Bank accoutn code

--- bank_git.rb
+++ bank_git.rb
@@ -1,5 +1,29 @@
-class Bank
-  
-end 
+class BankAccount
+  def initialize(owner, balance)
+    @owner = owner
+    @balance = balance
+  end
+  # Public method: anyone can call these
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    if sufficient_funds?(amount)
+      @balance -= amount
+    else
+      puts "Insufficient funds!"
+    end
+  end
+  private
+  # Private method: only the class itself can call this
+  def sufficient_funds?(amount)
+    amount <= @balance
+  end
+end
+
+bankAccount = BankAccount.new("Leon", 100)
+puts bankAccount.withdraw(50)
+puts bankAccount.deposit(100)
 
 #hello
