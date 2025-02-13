c683327b09eb1ab759c6adf56b84ff1ae1f59599
jessicaeri <jess.eri0926@gmail.com>
2025-02-13 19:27:07 +1000

    working add_account rspec

--- spec/banksystem_rspec.rb
+++ spec/banksystem_rspec.rb
@@ -2,7 +2,7 @@ require_relative '../bank_git'
 
 RSpec.describe BankSystem do
   let(:bank_system) {BankSystem.new}
-  # let (:ls_account) {BankAccount.new("Leon")}
+  let (:ls_account) {BankAccount.new("Leon")}
 
 
   describe '#initialize' do 
@@ -12,48 +12,40 @@ RSpec.describe BankSystem do
   end
 
 
-  describe '#add_account' do 
+  # describe '#add_account' do 
     # it 'adds an account from the bank_account class to the list_accounts array' do 
     #   expect(bank_system.add_account("ls_account"))
     # end
     
-    context 'when account is in the list_accounts array' do #context used in if.else functions
-      it 'displays a message' do 
-        ls_account = BankAccount.new("Leon")
-        expect { bank_system.add_account(ls_account) } .to output("Account already exists").to_stdout
-        expect(bank_system.add_account).to_not include('The Great Gatsby')
-      end
-    end
+    # context 'when account is in the list_accounts array' do #context used in if.else functions
+    #   it 'displays a message' do 
+    #     ls_account = BankAccount.new("Leon")
+    #     expect { bank_system.add_account(ls_account) } .to output("Account already exists").to_stdout expect(bank_system.list_accounts).to eq([ls_account])
+    #   end
+    # end
 
     # context 'when account is NOT in the list_accounts array' do 
     #   it 'adds the account to the list_accounts array and displays a message' do 
     #     account.owner
 
-   #   describe '#fetch_books_from_api' do
-#       context 'when the book is in the library' do
-#           it 'removes the book from the library and displays a message' do
-#               library.library << 'The Great Gatsby'
-#               expect { library.fetch_books_from_api('The Great Gatsby') }
-#                   .to output("Enjoy your book #{member}!\n").to_stdout
-#               expect(library.library).to_not include('The Great Gatsby')
-#           end
-#       end
+  describe '#add_account' do
+    context 'when the account already exists in list_accounts' do
+      it 'returns "Account already exists"' do
+        # First, add the account
+        bank_system.add_account(ls_account)
+        # Try adding the same account again
+        result = bank_system.add_account(ls_account)
+        expect(result).to eq("Account already exists")
+      end
+    end
 
-#       context 'when the book is not in the library' do
-#           it 'displays a book not found message' do
-#               expect { library.fetch_books_from_api('Moby Dick') }
-#                   .to output("Book not found within library.\n").to_stdout
-#               expect(library.library).to eq([ls_account])
-#           end
-#       end
-#   end
-# end
-    
-
-    #     expect(bank_system.add_account("ls_account")).to output("Account already exists")
-    #   end
-    # end
-
-  end
+    context 'when the account is NOT in list_accounts' do
+      it 'adds the account and returns a success message' do
+        result = bank_system.add_account(ls_account)
+        expect(result).to eq("Added account for Leon.")
+        expect(bank_system.list_accounts).to include(ls_account)
+      end
+    end
+  end               
 
 end
