a79268b2dd1d16f22ff0c8265f7c8e71bca3d8cc
CruzAlanna <lannikcru@gmail.com>
2025-02-13 20:11:50 +1000

    Add ratings/review functions, started Menu

--- movie_manager.rb
+++ movie_manager.rb
@@ -26,6 +26,30 @@ class Movie
   def initialize(title, director)
     @title = title
     @director = director
+    @stars = nil
+    @review = nil
+  end
+
+  def display_info
+    movie_info = "#{@title} (Directed by #{@director})"
+    if @stars
+      movie_info += " - #{@stars}/5 STARS"
+    end
+    if @review
+      movie_info += " - Review: #{@review}"
+    end
+  end
+
+  def set_rating(stars)
+    if stars >= 1 && stars <= 5
+      @stars = stars
+    else
+      return "Star rating must be between 1 and 5"
+    end
+  end
+
+  def set_review(review)
+    @review = review
   end
 end
 
@@ -62,11 +86,51 @@ class MovieLibrary
 end
 
 library = MovieLibrary.new
-library.add_movie("Movie name")
-library.add_movie("Movie name2")
-library.add_movie("Sharknado")
-# puts library.search_movie("Movie name2")
-# puts library.search_movie("Sharknado")
-# puts library.display
+puts "Welcome to the Movie Library!"
+puts "------------------------------"
+loop do
+  puts "Menu Selection \n 
+  1) Add a movie
+  2) Remove a movie
+  3) Search for a movie
+  4) Rate a movie
+  5) Leave a review
+  6) Display movie library
+  7) Display movie information
+  8) Exit"
+  puts "Input a number to choose an option."
+  option = gets.chomp.to_i
+  case option
+  when 1
+    puts "Enter movie title."
+    title = gets.chomp.capitalize
+    puts "Enter movie director."
+    director = gets.chomp.capitalize
+    movie = Movie.new(title, director)
+    library.add_movie(movie)
+    puts "#{title} added to library!"
 
-puts library.remove_movie("Movie name")
+  when 2
+    puts "Enter the movie title to remove."
+    title = gets.chomp
+    library.remove_movie(title)
+     puts "#{title} removed from library!"
+  end
+
+  break
+end
+
+
+# library.add_movie("Movie name")
+# library.add_movie("Movie name2")
+# library.add_movie("Sharknado")
+# # puts library.search_movie("Movie name2")
+# # puts library.search_movie("Sharknado")
+# # puts library.display
+
+# puts library.remove_movie("Movie name")
+
+# movie1 = Movie.new("Twilight", "Catherine Hardwicke")
+# movie1.set_rating(3)
+# movie1.set_review("Didnt appreciate it when it first came out, now its lowkey fun to watch for nostalgia")
+# puts movie1.display_info
